1ba89c84faefa3254c32946b17d7308cef065dc8
Aryaman Adhikari <aryamanrizz@gmail.com>
2025-06-18 08:15:18 +0545

    Create demoji.py

---
+++ demoji.py
@@ -0,0 +1,25 @@
+'''
+This will convert emoji to text
+
+'''
+import emoji
+
+
+def de_emoji(b):
+    
+
+
+
+    print(emoji.demojize(f'{b}'))
+
+    
+
+
+
+de_emoji('Python is 👍')
+   
+
+
+
+if __name__ == '__main__':
+    print("I am hidden")
