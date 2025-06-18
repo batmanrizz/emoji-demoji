dd331237a2d7f3368d74d38b4cb0bd07dd9958b5
Aryaman Adhikari <aryamanrizz@gmail.com>
2025-06-18 08:15:02 +0545

    Create emojiz.py

---
+++ emojiz.py
@@ -0,0 +1,20 @@
+''''
+
+This will convert text to emoji
+'''
+
+import emoji
+
+def emojiz(a):
+
+    print(emoji.emojize(f'{a}'))
+
+
+
+emojiz('Python is :thumbs_up:')
+
+
+
+
+if __name__ == '__main__':
+    print("I am hidden")
